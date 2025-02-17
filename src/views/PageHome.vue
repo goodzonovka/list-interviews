0ebf4f92ba1af359b03270b678df00b88eab4499
artem <goodzonovka@gmail.com>
2025-02-17 11:23:53 +0200

    test

--- src/views/PageHome.vue
+++ src/views/PageHome.vue
@@ -17,7 +17,7 @@ const contactTelegram = ref<string>('')
 const contactWhatsApp = ref<string>('')
 const contactPhone = ref<string>('')
 
-const test1;
+const test2;
 
 const loading = ref<boolean>(false)
 
