cc1f5366715e25621b512a64f236a8eb4abdd326
artem <goodzonovka@gmail.com>
2025-03-23 10:42:57 +0200

    deploy

--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,27 @@
-import { fileURLToPath, URL } from 'node:url'
+import { fileURLToPath, URL } from 'node:url';
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+import vueDevTools from 'vite-plugin-vue-devtools';
 
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-import vueDevTools from 'vite-plugin-vue-devtools'
-
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-    vueDevTools(),
-  ],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+    plugins: [
+        vue(),
+        vueDevTools(),
+    ],
+    resolve: {
+        alias: {
+            '@': fileURLToPath(new URL('./src', import.meta.url))
+        },
     },
-  },
-})
+    server: {
+        host: process.env.HOST || '127.0.0.1',
+        port: process.env.PORT ? Number(process.env.PORT) : 3000,
+        strictPort: true,
+        allowedHosts: ['interviews.goodzonovka.com'],
+    },
+    preview: {
+        host: '127.0.0.1', // Обязательно указываем хост
+        port: 3000, // Используем порт 3000
+        strictPort: true,
+    }
+});
